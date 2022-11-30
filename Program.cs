41cb5af0216f46cd72747bac712f21159342e6a5
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-30 16:15:48 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Given a string, write a method that checks if it is a palindrome. String length may be >= 0.
+
+
+
+namespace Program{
+    class IsPalindromeRecursionTask{
+            static string IsPalindromeRecursion(string str){
+                
+                 
+            }
+        static void Main(string [] args){
+            
+            Console.WriteLine(IsPalindromeRecursion("xx"));// → true
+            Console.WriteLine(IsPalindromeRecursion("pendrive"));// → false
+
+        }
+    }
+}
